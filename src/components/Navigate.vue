e3cf0f0a0ff25572eedeaf4d3727a939a14477c9
Larify <larify@outlook.com>
2024-02-17 17:51:45 +0800

    add daisyui component

--- src/components/Navigate.vue
+++ src/components/Navigate.vue
@@ -6,16 +6,23 @@ const urls = reactive(window.links);
 
 <template>
   <div class="flex-center flex-row flex-wrap gap-8">
-    <div
+    <a
       v-for="(item, index) in urls"
-      class="box-border flex-center flex-col hover:cursor-pointer card-item"
+      class="box-border flex-center flex-col hover:cursor-pointer no-underline card-item"
+      target="_blank"
+      :href="item.innerUrl"
       :key="index"
     >
-      <img :src="item.icon" alt="item.name" class="w-20 h-20 md:w-24 md:h-24 lg:w-28 lg:h-28" />
+      <img
+        class="rounded-full bg-white w-20 h-20 md:w-24 md:h-24 lg:w-28 lg:h-28"
+        :class="item.className || []"
+        :src="item.icon"
+        :alt="item.name"
+      />
       <div class="description font-medium text-slate-800">
         <span>{{ item.name }}</span>
       </div>
-    </div>
+    </a>
   </div>
 </template>
 
@@ -30,5 +37,9 @@ const urls = reactive(window.links);
 
 .card-item:hover {
   transform: scale(1.2);
+}
+
+.class-name {
+  @apply p-1 p-2 p-3 p-4 p-5 p-6;
 }
 </style>
