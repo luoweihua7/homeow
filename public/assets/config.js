afdf67abd5159f0ac4b39b233066347ba693a42f
Larify <larify@outlook.com>
2024-02-17 17:51:52 +0800

    update config

--- public/assets/config.js
+++ public/assets/config.js
@@ -1,59 +1,81 @@
 (() => {
-  const baseIconUrl = './assets/svg';
-  const links = [
+  window.links = [
     {
-      icon: 'Settings.svg',
-      name: '设置',
+      icon: './assets/icons/mikrotik.png',
+      name: 'Mikrotik',
+      innerUrl: 'http://10.0.0.1/',
+      outerUrl: '',
     },
     {
-      icon: 'AppStore.svg',
-      name: 'AppStore',
+      icon: './assets/icons/openwrt.svg',
+      name: 'OpenWrt',
+      className: ['p-2'],
+      innerUrl: 'http://10.0.0.2/',
+      outerUrl: '',
     },
     {
-      icon: 'Downloads.svg',
-      name: '下载',
+      icon: './assets/icons/proxmox.png',
+      name: 'Proxmox VE',
+      className: ['p-1'],
+      innerUrl: 'https://10.0.0.20:8006/',
+      outerUrl: '',
     },
     {
-      icon: 'Emby.svg',
-      name: 'AppStore',
+      icon: './assets/icons/qnap.png',
+      name: 'QNAP',
+      innerUrl: 'http://10.0.0.10:5000/',
+      outerUrl: '',
     },
     {
-      icon: 'Downloads-1.svg',
-      name: 'AppStore',
+      icon: './assets/icons/QuMagic.png',
+      name: 'QuMagic',
+      className: ['p-2'],
+      innerUrl: 'http://10.0.0.10:5000/qumagie/',
+      outerUrl: '',
     },
     {
-      icon: 'Plex.svg',
-      name: 'AppStore',
+      icon: './assets/icons/portainer.png',
+      name: 'Portainer',
+      innerUrl: 'http://10.0.0.10:9000/',
+      outerUrl: '',
     },
     {
-      icon: 'Downloads-3.svg',
-      name: 'AppStore',
+      icon: './assets/icons/plex.svg',
+      name: 'Plex',
+      innerUrl: 'http://10.0.0.10:32400/',
+      outerUrl: '',
     },
     {
-      icon: 'Nginx-1.svg',
-      name: 'AppStore',
+      icon: './assets/icons/emby.svg',
+      name: 'Emby',
+      className: ['p-2'],
+      innerUrl: 'http://10.0.0.10:8096',
+      outerUrl: '',
     },
     {
-      icon: 'Downloads-2.svg',
-      name: 'AppStore',
+      icon: './assets/icons/HomeAssistant.png',
+      name: 'HomeAssistant',
+      innerUrl: 'http://10.0.0.10:8123/',
+      outerUrl: '',
     },
     {
-      icon: 'AppStore.svg',
-      name: 'AppStore',
+      icon: './assets/icons/alist.svg',
+      name: 'AList',
+      className: ['p-1'],
+      innerUrl: 'http://10.0.0.10:15244/',
+      outerUrl: '',
     },
-    {
-      icon: 'Emby.svg',
-      name: 'AppStore',
-    },
-    {
-      icon: 'AppStore.svg',
-      name: 'AppStore',
-    },
+    // {
+    //   icon: './assets/icons/music.svg',
+    //   name: '音乐',
+    //   innerUrl: '',
+    //   outerUrl: '',
+    // },
+    // {
+    //   icon: './assets/icons/bitwarden-v2.svg',
+    //   name: 'Bitwarden',
+    //   innerUrl: '',
+    //   outerUrl: '',
+    // },
   ];
-
-  window.links = links.map((item) => {
-    item.icon = `${baseIconUrl}/${item.icon}`;
-
-    return item;
-  });
 })();
